95751ca09dbf685798dd8a1531ccb671e1129426
MuratYucedag <yucedagmurat23@gmail.com>
2023-07-12 10:42:55 +0300

    Add project files.

---
+++ Casgem_CodeFirstProject/DAL/Entities/SocialMedia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Casgem_CodeFirstProject.DAL.Entities
+{
+    public class SocialMedia
+    {
+        public int SocialMediaID { get; set; }
+        public string SocialMediaName{ get; set; }
+        public string SocialMediaUrl{ get; set; }
+    }
+}
